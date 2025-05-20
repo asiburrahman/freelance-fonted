56eede1eb526a07264e8767d2701901c892529fa
Asibur Rahman <asibur70@gmail.com>
2025-05-20 16:38:27 +0600

    add user design and show my task

--- src/component/Pages/AddTask.jsx
+++ src/component/Pages/AddTask.jsx
@@ -1,9 +1,191 @@
-import React from 'react';
+import React, { use, useState } from 'react';
+import "react-clock/dist/Clock.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { AuthContext } from '../../context/AuthContext';
+import Swal from 'sweetalert2';
 
 const AddTask = () => {
+    const [startDate, setStartDate] = useState(new Date());
+    const {user} = use(AuthContext)
+    console.log(user);
+    
+
+
+    const handleAddSchedule = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const data = Object.fromEntries(formData.entries());
+        const date = startDate.toLocaleDateString("en-CA");
+        const userData = {date ,...data}
+        console.log(userData);
+
+
+        fetch('http://localhost:3000/task', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(userData)
+                }).then(res => res.json()).then(data => {
+                    if (data.insertedId) {
+                        
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: "Your Task has been saved",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+        
+                        form.reset()
+                    }
+        })
+        
+    };
+
+
     return (
         <div>
-            add task
+            <div className="bg-[#F4F3F0] lg:p-24">
+                <h2 className="text-3xl text-center font-bold">Add Gym Schedule</h2>
+                <form onSubmit={handleAddSchedule}>
+
+                    <div className="flex flex-col gap-6 ">
+
+
+
+                        { /* Task Title */}
+                        <div className="form-control md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-bold">Title</span>
+                            </label>
+                            <input
+                                type="text"
+                                name="title"
+                                placeholder="Title"
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
+
+                        {/* Category  */}
+                        <div className="form-control md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-bold">Category</span>
+                            </label>
+
+                            <select className="input input-bordered w-full" name="category" id="day">
+                                <option value="Web Development">Web Development</option>
+                                <option value="Graphics Design">Graphics Design</option>
+                                <option value="Content Writing">Content Writing</option>
+                                <option value="Digital Marketing">Digital Marketing</option>
+                                <option value="Video Editing">Video Editing</option>
+                                {/* <option value="friday">Friday</option>
+                <option value="saturday">Saturday</option> */}
+                            </select>
+                        </div>
+
+                        {/* Description */}
+                        <div className="form-control md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-bold">Description</span>
+                            </label>
+                            <textarea
+                                type="text"
+                                name="description"
+                                placeholder="Description"
+                                className="input input-bordered w-full h-20"
+                                required
+                            />
+                        </div>
+
+
+                        {/* Deadline */}
+                        <div className="form-control lg:w-1/2 mt-6 md:mt-0">
+                            <label className="label font-bold">
+                                <span className="label-text">Deadline</span>
+                            </label>
+                            <DatePicker
+                                className="input input-bordered w-full"
+                                selected={startDate}
+                                onChange={(date) => setStartDate(date)}
+                            />
+                        </div>
+
+
+                        {/* Budget  */}
+
+                        <div className="form-control md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-bold">Budget</span>
+                            </label>
+                            <input
+                                type="number"
+                                name="budget"
+                                placeholder="Price"
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
+
+                        {/* User Name  */}
+                        <div className="form-control md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-bold">User Name</span>
+                            </label>
+                            <input
+                                type="Text"
+                                name="name"
+                                value={user.displayName}
+                                placeholder="User Name"
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
+
+                        {/* User Name  */}
+                        <div className="form-control md:w-1/2">
+                            <label className="label">
+                                <span className="label-text font-bold">User Mail</span>
+                            </label>
+                            <input
+                                type="Email"
+                                name="email"
+                                 value={user.email}
+                                placeholder="Email"
+                                className="input input-bordered w-full"
+                                required
+                            />
+                        </div>
+
+                        {/* <div className="form-control lg:w-1/2 mt-6 md:mt-0">
+              <label className="label font-bold">
+                <span className="label-text">Time</span>
+              </label>
+
+              <DatePicker
+                className="input input-bordered w-full"
+                selected={selectedTime}
+                onChange={handleTimeChange}
+                showTimeSelect
+                showTimeSelectOnly
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat="h:mm aa"
+              />
+            </div> */}
+                    </div>
+
+                    {/* End of Labels */}
+                    <input
+                        type="submit"
+                        value="Add Schedule"
+                        className="btn w-full bg-pink-500 text-white mt-6"
+                    />
+                </form>
+            </div>
         </div>
     );
 };
--- src/component/Pages/MyPostedTask.jsx
+++ src/component/Pages/MyPostedTask.jsx
@@ -1,9 +1,129 @@
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import Loading from '../Loading/Loading';
 
 const MyPostedTask = () => {
+    const {user, loading, setLoading} = use(AuthContext)
+    const [tasks, setTask] = useState([])
+    
+
+
+
+
+    useEffect(()=>{
+                
+                // console.log(location);
+               fetch(`http://localhost:3000/task/${user.email}`,{
+                method:"GET"
+               }).then(res=> res.json()).then(data=>setTask(data)
+               )
+        
+                
+            },[user])
+
+            
+            
+            
+            console.log(tasks);
+            
+
+
+
     return (
         <div>
-            My Posted Task
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Deadline</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            tasks.map( (task, index) => <tr>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle h-12 w-12">
+                                            <img
+                                                src={user.photoURL}
+                                                alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">{user.displayName}</div>
+                                        <div className="text-sm opacity-50">United States</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td >
+                                {task.title}
+                                <br />
+                                <span className="badge badge-ghost badge-sm">Rate: {task.budget}$</span>
+                            </td>
+                            
+                            <td>
+                                {task.category}
+                            </td>
+                            <td>
+                                {task.date}
+                            </td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">Update</button>
+                                <button className="btn btn-ghost btn-xs">Delete</button>
+                                <button className="btn btn-ghost btn-xs">Bids</button>
+                            </th>
+                        </tr>)
+                        }
+                        
+                        {/* <tr>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle h-12 w-12">
+                                            <img
+                                                src={user.photoURL}
+                                                alt="Avatar Tailwind CSS Component" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">{user.displayName}</div>
+                                        <div className="text-sm opacity-50">United States</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Zemlak, Daniel and Leannon
+                                <br />
+                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                            </td>
+                            <td>Purple</td>
+                            <td>
+                                <p>11-11-11</p>
+                            </td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">details</button>
+                                <button className="btn btn-ghost btn-xs">details</button>
+                                <button className="btn btn-ghost btn-xs">details</button>
+                            </th>
+                        </tr> */}
+                       
+                    </tbody>
+                    {/* foot */}
+                    <tfoot>
+                        <tr>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                            <th></th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
         </div>
     );
 };
